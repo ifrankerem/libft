2a6a0e6e6e2adb1fa7012904e90f0eafe779a3d0
İrfan Kerem Arslan <121255615+ifrankerem@users.noreply.github.com>
2024-10-25 13:42:05 +0000

    split change

--- src/ft_split.c
+++ src/ft_split.c
@@ -3,67 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iarslan <iarslan@student.42istanbul.com    +#+  +:+       +#+        */
+/*   By: iarslan <iarslan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 18:43:23 by iarslan           #+#    #+#             */
-/*   Updated: 2024/10/15 20:16:57 by iarslan          ###   ########.fr       */
+/*   Updated: 2024/10/25 13:41:20 by iarslan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int	ft_alphcount(char const *x, char y)
-{
-	int	i;
-	int	flag;
-
-	flag = 0;
-	i = 0;
-	while (x[i] == y)
-		i++;
-	while (x[i])
-	{
-		if (x[i] != y)
-		{
-			flag++;
-			i++;
-		}
-        else
-            i++;
-	}
-	return (flag);
-}
-char **ft_copy(char const *s, char c)
-{
-	char **final;
-	int i;
-	int j;
-	int k;
-	j = 0;
-	int size;
-	int wordnumber;
-	wordnumber =  ft_wordcount(s, c);
-	int 
-	i = 0;
-	size = ft_alphcount(s,c);
-	*final = malloc(sizeof(char) * (size +wordnumber));
-	while(s[i])
-	{	
-		j = 0;
-		while(s[i] == c)
-		{	
-			final[k][j] = s[i];
-			i++;
-			j++;
-		}
-		final[k][j] = '\0';
-		k++;
-	}
-	return(final);
-}
-
-int	ft_wordcount(char const *x, char y)
+static int	ft_wordcount(char const *x, char y)
 {
 	int	i;
 	int	flag;
@@ -78,14 +28,51 @@ int	ft_wordcount(char const *x, char y)
 	}
 	return (flag);
 }
-char	**ft_split(char const *s, char c)
+static char *copy_word(const char *s, int start, int end)
+{
+	char *word;
+	int j;
+	j = 0;
+
+	word = malloc(sizeof(char) * (end - start + 1));
+	if (!word)
+		return (NULL);
+	while (start < end)
+		word[j++] = s[start++];
+	word[j] = '\0';
+	return (word);
+}
+
+char **ft_copy(char const *s, char c)
 {
 	char **final;
-	int wordcount;
-	wordcount = ft_wordcount(s, c);
-	*final = malloc(sizeof(char *) * wordcount);
-    final = ft_copy(s,c);
-	return(final);
+	int i;
+	int k;
+	i = 0;
+	k = 0;
+	final = malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
+	if (!final)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			int start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			final[k++] = copy_word(s, start, i);
+		}
+		else
+			i++;
+	}
+	final[k] = NULL;
+	return (final);
+}
+char **ft_split(char const *s, char c)
+{
+	if (!s)
+		return (NULL);
+	return (ft_copy(s, c));
 }
 int main()
 {	
